7291ff273157dc47b96e93f0aac780465d47cb7c
Jaksic98 <45017005+Jaksic98@users.noreply.github.com>
2024-07-23 00:01:11 +0200

    Creating spices page

--- assignment/src/app/app.component.ts
+++ assignment/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SpicesComponent } from './pages/spices/spices.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SpicesComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'assignment';
---
+++ assignment/src/app/pages/spices/spices.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-spices',
+  standalone: true,
+  imports: [],
+  templateUrl: './spices.component.html',
+  styleUrl: './spices.component.scss',
+})
+export class SpicesComponent {}
