2cefe5a3616645d1931df6d133a0dc9ba9a150ad
Jaksic98 <45017005+Jaksic98@users.noreply.github.com>
2024-07-24 20:46:41 +0200

    Reformated list and table to use arrays and objects

--- assignment/src/app/pages/spices/spices.component.ts
+++ assignment/src/app/pages/spices/spices.component.ts
@@ -1,10 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+export interface Table {
+  header: string[];
+  rows: number[][];
+}
 
 @Component({
   selector: 'app-spices',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './spices.component.html',
   styleUrl: './spices.component.scss',
 })
-export class SpicesComponent {}
+export class SpicesComponent {
+  spices: any = [
+    'biber',
+    ['beli', 'crni', 'zeleni'],
+    'šafran',
+    'kurkuma',
+    'kardamon',
+  ];
+
+  production: Table = {
+    header: ['Indija', 'Bangladeš', 'Turska', 'Kina', 'Pakistan'],
+    rows: [[70, 9, 5.7, 5.5, 3]],
+  };
+
+  isArray(spice: string | string[]): boolean {
+    console.log(Array.isArray(spice));
+    return Array.isArray(spice);
+  }
+}
